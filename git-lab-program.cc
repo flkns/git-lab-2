d2297c73db658e4a972ec62c8dbbb60e7eaaf766
Benjamin Folkens <bf853817@ohio.edu>
2021-09-01 17:11:58 -0400

    added git-lab-program.cc + a.out

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+ *  File:           git-lab-program.cc
+ *  Author:         Benjamin Folkens
+ *  Date:           September 1st, 2021
+ *  Description:     
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
